8850c386546c144f80a42ca2065500d7f26603dd
Artem Cherepanov <artemcherepanov2357@gmail.com>
2025-07-24 20:53:11 +0000

    step 2. build

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,11 @@ def generate_diff():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output',
+        default='stylish',
+    )
     args = parser.parse_args()
     return args
 
