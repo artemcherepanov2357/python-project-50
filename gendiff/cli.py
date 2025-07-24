0b0ead13854dc855c853d1746dc56c79acb833cb
Artem Cherepanov <artemcherepanov2357@gmail.com>
2025-07-24 13:01:03 +0000

    init

---
+++ gendiff/cli.py
@@ -0,0 +1,9 @@
+from gendiff.scripts.gendiff import generate_diff
+
+
+def main():
+    generate_diff()
+
+
+if __name__ == '__main__':
+    main()
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+import argparse
+
+
+def generate_diff():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    generate_diff()
+
+
+if __name__ == '__main__':
+    main()
