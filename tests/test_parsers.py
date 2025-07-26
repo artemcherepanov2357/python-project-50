fa14321abf558572d2ffa83c02608390117932b3
Artem Cherepanov <artemcherepanov2357@gmail.com>
2025-07-26 21:04:23 +0000

    step 6. - workflow_dispatch

---
+++ tests/test_parsers.py
@@ -0,0 +1,19 @@
+import pytest
+from gendiff.parsers import parse, get_extension
+
+@pytest.mark.parametrize("extension, data", [
+    ('.json', '{"key": "value"}'),
+    ('.yml', 'key: value'),
+    ('.yaml', 'key: value')
+])
+def test_parse(extension, data):
+    result = parse(data, extension)
+    assert result == {"key": "value"}
+
+def test_parse_unsupported_format():
+    with pytest.raises(ValueError):
+        parse("data", ".txt")
+
+def test_get_extension():
+    assert get_extension("/path/to/file.json") == ".json"
+    assert get_extension("/path/to/file.YML") == ".yml"
