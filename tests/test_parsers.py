03b22fcf2457157f7c104dcb9cd579c999ee958a
Artem Cherepanov <artemcherepanov2357@gmail.com>
2025-07-26 21:05:45 +0000

    step 6. - workflow_dispatch fix

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,5 +1,7 @@
 import pytest
-from gendiff.parsers import parse, get_extension
+
+from gendiff.parsers import get_extension, parse
+
 
 @pytest.mark.parametrize("extension, data", [
     ('.json', '{"key": "value"}'),
@@ -10,9 +12,11 @@ def test_parse(extension, data):
     result = parse(data, extension)
     assert result == {"key": "value"}
 
+
 def test_parse_unsupported_format():
     with pytest.raises(ValueError):
         parse("data", ".txt")
+
 
 def test_get_extension():
     assert get_extension("/path/to/file.json") == ".json"
